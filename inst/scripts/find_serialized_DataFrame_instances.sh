bd01eac317ee9f55eefe1065b418d69ce21f82b0
Hervé Pagès <hpages.on.github@gmail.com>
2022-01-13 21:46:39 -0500

    small tweak to inst/scripts/find_serialized_DataFrame_instances.sh

--- inst/scripts/find_serialized_DataFrame_instances.sh
+++ inst/scripts/find_serialized_DataFrame_instances.sh
@@ -4,6 +4,23 @@
 #
 #   path/to/find_serialized_DataFrame_instances.sh >find_serialized_DataFrame_instances.log 2>&1 &
 #
+# Once the script has completed, extract the list of packages with old
+# DataFrame instances with:
+#
+#   grep '^File .* [1-9][0-9]*$' find_serialized_DataFrame_instances.log | \
+#     sed 's/^File //' | cut -d '/' -f 1 | sort | uniq
+#
+# This script was successfully run on nebbiolo1 (BioC 3.15, 2076 packages in
+# manifest) on Jan 13, 2022:
+# - log file: find_serialized_DataFrame_instances-20220113.log (located in
+#   this folder)
+# - 2076 input packages (2064 processed, 12 skipped)
+# - took about 25 min
+# - attached or loaded 1191 packages (325 attached and 866 loaded) as
+#   reported by sessionInfo()
+# - required about 8.5 Gb of RAM
+# - found 24 packages with old DataFrame instances (run the above grep
+#   command on the log file to extract the list)
 
 set -e  # exit immediately if a simple command returns a non-zero status
 
@@ -11,7 +28,7 @@ set -e  # exit immediately if a simple command returns a non-zero status
 MEAT0_DIR="/home/biocbuild/bbs-3.15-bioc/MEAT0"
 Rscript="/home/biocbuild/bbs-3.15-bioc/R/bin/Rscript"
 
-SKIPPED_PKGS="c('BaalChIP', 'BiGGR', 'ChIPpeakAnno', 'CytoTree', 'GeneAnswers', 'gwascat', 'isobar', 'mirIntegrator', 'oposSOM', 'PFP', 'ROntoTools', 'SLGI', 'SNPhood')"
+SKIPPED_PKGS="c('BaalChIP', 'BiGGR', 'ChIPpeakAnno', 'CytoTree', 'gwascat', 'isobar', 'mirIntegrator', 'oposSOM', 'PFP', 'ROntoTools', 'SLGI', 'SNPhood')"
 FILTER="\\\\bDataFrame\\\\b"
 CODES_DEST_FILE="find_serialized_DataFrame_instances_codes.rds"
 
